68e2edfae55695d3269a696d75adcdc26e8592a2
Jinsu-J <jjs990422@gmail.com>
2022-12-09 23:42:00 -0800

    Jinsu

--- A5/server.R
+++ A5/server.R
@@ -2,27 +2,106 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 library(ggplot2)
+library(dplyr)
+library(data.table)
+library(janitor)
 
 df_1 <- read.csv("owid-co2-data.csv")
 
 countries <- unique(df_1$country)
-min_year <- min(df_1$year)
 max_year <- max(df_1$year)
+start_year <- 1960
+year_1960 <- df_1 %>%
+  filter(year >= 1960)
 
-shinyServer(function(input, output) {
+table_search <- df_1 %>%
+  group_by(year) %>%
+  select(year, country, co2, co2_per_capita, co2_per_gdp, total_ghg)
+
+server <- function(input, output, session) {
   
+  output$bar <- renderPlotly({
+    selected_country <- input$countries
+    if (selected_country != 'Afghanistan') {
+      df_2 <- df_1 %>%
+        group_by(year) %>%
+        select(year, country, co2) %>%
+        filter(country == selected_country)
+    }
+    else {
+      df_2 <- df_1 %>%
+        group_by(year) %>%
+        select(year, country, co2) %>%
+        filter(country == "Afghanistan") 
+    }
+    ggplotly(
+      print(ggplot(df_2)) +
+        geom_bar(mapping = aes(x = year, y = co2), stat = "identity") +
+        labs(title = "Annual production-based emssions of CO2") +
+        scale_x_continuous(limits = c(input$year[1], input$year[2]),
+                           breaks = seq(start_year, max_year, 3)) +
+        ylab("CO2 in million tonnes") +
+        theme(axis.text.x = element_text(angle = 50, hjust = 1, size = 5))
+    )
+    
+   })
+  
+  output$country_name <- renderUI({
+    selectInput(
+      "country_select",
+      "Type the countries you want to compare:",
+      choices = as.character(df_1$country),
+      multiple = TRUE,
+      selected = "Croatia"
+    )
+  })
+  
+  output$year_selected <- renderUI({
+    sliderInput("inputyear", "Which year:", 
+                min = start_year, max = max_year,
+                value = c(start_year, max_year), sep = "",)
+  })
+  
+  output$data_cal <- renderUI({
+    selectInput(
+      "total_mean",
+      "Choose type of calculation",
+      choices = c("Total", "Mean")
+    )
+  })
+  
+  
+  data_subset = reactive({
+    table_search %>%
+    subset(country %in% input$country_select) %>%
+    subset(year >= input$inputyear[1] &
+             year <= input$inputyear[2])
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+  })
+  
+  data_total = reactive({
+  selected_cal <- input$total_mean
+  if (selected_cal != 'Total'){
+    table_search %>%
+      subset(country %in% input$country_select) %>%
+      subset(year >= input$inputyear[1] &
+               year <= input$inputyear[2]) %>%
+      adorn_totals(name = "mean") %>%
+      mutate(across(where(is.numeric), 
+                    ~ replace(., n(), .[n()]/(n()-1))))
+    
+  } else {
+    table_search %>%
+      subset(country %in% input$country_select) %>%
+      subset(year >= input$inputyear[1] &
+               year <= input$inputyear[2]) %>%
+      adorn_totals("row")}
+    
 })
+  
+  
+  output$data_Table <- renderTable(data_subset())
+  output$sum_total <- renderTable(data_total())
+
+}
+
--- A5/ui.R
+++ A5/ui.R
@@ -1,13 +1,8 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(shinythemes)
+library(plotly)
+library(data.table)
 source("server.R")
 
 fpage <- tabPanel(
@@ -20,13 +15,43 @@ fpage <- tabPanel(
     selectInput("countries", "Select a country",
                 choices = countries,
                 ),
-    sliderInput("year", "Year Range",
+   sliderInput("year", "Year Range",
+                min = start_year,
                 max = max_year,
-                value = c(min_year, max_year),
-                step = 1)
-    ),
+                value = c(start_year, max_year),
+                sep = ""),
+   ),
   mainPanel(
     plotlyOutput("bar"),
+    h4(p("This visualization shows the amount of CO2 emission from each
+         country in different time period. Users can learn the values of 
+         each country's co2 emission in the time period that they investigate. 
+         To use this page in-depth for learning, you can compare and contrast 
+         countries as many as you want in the first page, and can visualize the
+         co2 emission from specific country in this page."))
     )
   )
 )
+
+searchbar <- tabPanel (
+  uiOutput("country_name"),
+  sidebarLayout(
+    sidebarPanel(
+  uiOutput("year_selected"),
+  uiOutput("data_cal")
+    ),
+  mainPanel(
+    tabsetPanel(
+    tabPanel("Calculation", tableOutput("sum_total"))
+    ),
+  )))
+
+
+ui <- navbarPage(
+  title = "Investigate CO2 emission",
+  theme = shinytheme("cyborg"),
+  searchbar,
+  fpage
+)
+
+shinyApp(ui = ui, server = server)
